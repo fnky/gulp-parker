ddf6ee5f3a52a7573a197cfb6becb9b9d82d7088
Christian Petersen <humanoidism@gmail.com>
2015-02-06 15:24:15 +0100

    Implement functional tests

--- tests/test.js
+++ tests/test.js
@@ -8,33 +8,7 @@ var parker = require('..');
 describe('parker()', function() {
   it('should analyse files', function(cb) {
 
-    var stream = fs.createReadStream('./tests/css/a.css')
-                   .pipe(parker());
-
-    var buffer = [];
-
-    stream.on('data', function(file) {
-      buffer.push(file);
-    });
-
-    stream.on('end', function() {
-      console.log(buffer);
-      assert.equal(buffer.length, 1);
-      cb();
-    });
-
-    stream.end();
-  });
-
-  /*it('should analyse files from a set of metrics', function(cb) {
-
-    var stream = parker({
-      metrics: [
-        'TotalRules',
-        'TotalStylesheets'
-      ]
-    });
-
+    var stream = parker({ format: 'object' });
     var buffer = [];
 
     stream.on('data', function(file) {
@@ -43,21 +17,45 @@ describe('parker()', function() {
 
     stream.on('end', function() {
       assert.equal(buffer.length, 1);
-      cb();
+      assert.equal(buffer[0]['unique-colours'][0], '#AAAAAA');
+      cb()
     });
+
+    stream.write(new gutil.File({
+      cwd: __dirname,
+      base: __dirname + '/fixture',
+      path: __dirname + '/fixture/fixture.css',
+      contents: new Buffer('body { color: #aaa }')
+    }));
 
     stream.end();
-  });*/
-
-  /*it('should format as human readable text', function(cb) {
-    var stream = parker({
-      format: 'human'
-    });
   });
 
-  it('should use numeric agenda', function(cb) {
-    var stream = parker({
-      format: 'human'
+  it('should analyse files by a set of metrics', function(cb) {
+
+    var stream = parker({ metrics: [
+      'UniqueColours',
+      'TotalStylesheets'
+    ], format: 'object' });
+    var buffer = [];
+
+    stream.on('data', function(file) {
+      buffer.push(file);
     });
-  });*/
+
+    stream.on('end', function() {
+      assert.equal(buffer.length, 1);
+      assert.deepEqual(buffer[0], { 'total-stylesheets': 1, 'unique-colours': [ '#AAAAAA' ] });
+      cb()
+    });
+
+    stream.write(new gutil.File({
+      cwd: __dirname,
+      base: __dirname + '/fixture',
+      path: __dirname + '/fixture/fixture.css',
+      contents: new Buffer('body { color: #aaa }')
+    }));
+
+    stream.end();
+  });
 });
